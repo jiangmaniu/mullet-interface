904a82b51fd9b7f20a69ae3df78df331d819f495
poetries <jingguanliuye@gmail.com>
2024-09-09 21:39:50 +0800

    fix: fix bug

--- src/loading.tsx
+++ src/loading.tsx
@@ -2,15 +2,17 @@ import { PageLoading } from '@ant-design/pro-components'
 import { ConfigProvider } from 'antd'
 
 import { gray } from './theme/theme.config'
+import { STORAGE_GET_THEME } from './utils/storage'
 
 // 全局加载组件
 // Umi 4 默认 按页分包 ，从而在页面切换时存在加载过程，通过该文件来配置加载动画
 export default () => {
+  const isDark = STORAGE_GET_THEME() === 'dark'
   return (
     <ConfigProvider
       theme={{
         token: {
-          colorPrimary: gray['900']
+          colorPrimary: isDark ? '#f4f4f4' : gray['900']
         }
       }}
     >
