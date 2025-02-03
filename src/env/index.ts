441332c79485070676998e708a74463549b45a56
poetries <jingguanliuye@gmail.com>
2025-02-03 13:37:20 +0800

    fix: fix env

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,8 +2,6 @@ import { IPlatformConfig } from '@/mobx/global'
 import { STORAGE_GET_PLATFORM_CONFIG } from '@/utils/storage'
 import serverConf from './server'
 
-const ENV = STORAGE_GET_PLATFORM_CONFIG() || {}
-
 export const getEnv = () => {
   // 客户端环境变量
   const clientConf = STORAGE_GET_PLATFORM_CONFIG() || {}
@@ -14,5 +12,3 @@ export const getEnv = () => {
   }
   return env as IPlatformConfig
 }
-
-export default ENV
