44f0918fbe21d9ad2cd5053ebbf656660ff7756d
poetries <jingguanliuye@gmail.com>
2025-02-10 10:09:25 +0800

    feat: k线使用项目集成的

--- src/pages/web/trade/comp/TradingView/index.bak.tsx
+++ src/pages/web/trade/comp/TradingView/index.bak.tsx
@@ -1,66 +1,66 @@
-// eslint-disable-next-line simple-import-sort/imports
-import { observer } from 'mobx-react'
-import { useEffect, useMemo, useState } from 'react'
+// // eslint-disable-next-line simple-import-sort/imports
+// import { observer } from 'mobx-react'
+// import { useEffect, useMemo, useState } from 'react'
 
-import { getTradingViewLng } from '@/constants/enum'
-import { useEnv } from '@/context/envProvider'
-import { useLang } from '@/context/languageProvider'
-import { useStores } from '@/context/mobxProvider'
-import { getEnv } from '@/env'
-import { Spin } from 'antd'
+// import { getTradingViewLng } from '@/constants/enum'
+// import { useEnv } from '@/context/envProvider'
+// import { useLang } from '@/context/languageProvider'
+// import { useStores } from '@/context/mobxProvider'
+// import { getEnv } from '@/env'
+// import { Spin } from 'antd'
 
-type IProps = {
-  style?: React.CSSProperties
-}
+// type IProps = {
+//   style?: React.CSSProperties
+// }
 
-const TradingViewComp = ({ style }: IProps) => {
-  const ENV = getEnv()
-  const { isPc } = useEnv()
-  const { lng } = useLang()
-  const [tradeUrl, setTradeUrl] = useState('')
-  const { trade } = useStores()
-  const [loading, setLoading] = useState(false)
+// const TradingViewComp = ({ style }: IProps) => {
+//   const ENV = getEnv()
+//   const { isPc } = useEnv()
+//   const { lng } = useLang()
+//   const [tradeUrl, setTradeUrl] = useState('')
+//   const { trade } = useStores()
+//   const [loading, setLoading] = useState(false)
 
-  const activeSymbolName = trade.activeSymbolName
-  const symbolInfo = trade.getActiveSymbolInfo(trade.activeSymbolName, trade.symbolListAll)
-  const dataSourceCode = symbolInfo?.dataSourceCode
-  const dataSourceSymbol = symbolInfo?.dataSourceSymbol
+//   const activeSymbolName = trade.activeSymbolName
+//   const symbolInfo = trade.getActiveSymbolInfo(trade.activeSymbolName, trade.symbolListAll)
+//   const dataSourceCode = symbolInfo?.dataSourceCode
+//   const dataSourceSymbol = symbolInfo?.dataSourceSymbol
 
-  useEffect(() => {
-    if (dataSourceSymbol) {
-      setLoading(true)
-      setTradeUrl(
-        `${
-          ENV.tradingViewUrl
-        }/?lang=${getTradingViewLng()}&dataSourceSymbol=${dataSourceSymbol}&dataSourceCode=${dataSourceCode}&symbolName=${
-          symbolInfo?.symbol
-        }`
-      )
-    }
-  }, [lng, activeSymbolName, dataSourceCode])
+//   useEffect(() => {
+//     if (dataSourceSymbol) {
+//       setLoading(true)
+//       setTradeUrl(
+//         `${
+//           ENV.tradingViewUrl
+//         }/?lang=${getTradingViewLng()}&dataSourceSymbol=${dataSourceSymbol}&dataSourceCode=${dataSourceCode}&symbolName=${
+//           symbolInfo?.symbol
+//         }`
+//       )
+//     }
+//   }, [lng, activeSymbolName, dataSourceCode])
 
-  const iframeDom = useMemo(() => {
-    return (
-      <Spin spinning={loading} size="large">
-        <iframe
-          src={tradeUrl}
-          style={{
-            border: 'none',
-            height: isPc ? '591px' : 'calc(100vh - 260px)',
-            width: '100%',
-            visibility: loading ? 'hidden' : 'visible'
-          }}
-          onLoad={() => {
-            setTimeout(() => {
-              setLoading(false)
-            }, 100)
-          }}
-        />
-      </Spin>
-    )
-  }, [tradeUrl, loading])
+//   const iframeDom = useMemo(() => {
+//     return (
+//       <Spin spinning={loading} size="large">
+//         <iframe
+//           src={tradeUrl}
+//           style={{
+//             border: 'none',
+//             height: isPc ? '591px' : 'calc(100vh - 260px)',
+//             width: '100%',
+//             visibility: loading ? 'hidden' : 'visible'
+//           }}
+//           onLoad={() => {
+//             setTimeout(() => {
+//               setLoading(false)
+//             }, 100)
+//           }}
+//         />
+//       </Spin>
+//     )
+//   }, [tradeUrl, loading])
 
-  return <div style={style}>{iframeDom}</div>
-}
+//   return <div style={style}>{iframeDom}</div>
+// }
 
-export default observer(TradingViewComp)
+// export default observer(TradingViewComp)
