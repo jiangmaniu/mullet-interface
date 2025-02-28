04eabfe78ea35bb96d40230df65deef9d2b559e0
will <w251754731@gmail.com>
2025-02-28 17:22:52 +0800

    feat: 入金上傳憑證

--- src/pages/webapp/pages/Deposit/Otc/comp/UploadCard.tsx
+++ src/pages/webapp/pages/Deposit/Otc/comp/UploadCard.tsx
@@ -4,11 +4,12 @@ import { ProFormText } from '@ant-design/pro-components'
 import { FormattedMessage } from '@umijs/max'
 import type { UploadProps } from 'antd'
 import { Image, Upload } from 'antd'
-import { useState } from 'react'
 
 import { getEnv } from '@/env'
 import { message } from '@/utils/message'
 import { STORAGE_GET_TOKEN, STORAGE_GET_USER_INFO } from '@/utils/storage'
+import { useCallback, useEffect, useState } from 'react'
+import { isAndroid, isIOS } from 'react-device-detect'
 
 const { Dragger } = Upload
 
@@ -18,11 +19,16 @@ type IProps = {
 }
 
 export default function ({ setImgs, imgs }: IProps) {
-  const [fileName, setFileName] = useState('') // 文件名路径
-  const [url, setUrl] = useState('') // 完整地址
+  // const [fileName, setFileName] = useState('') // 文件名路径
+  // const [url, setUrl] = useState('') // 完整地址
 
   const userInfo = STORAGE_GET_USER_INFO() as User.UserInfo
   const token = STORAGE_GET_TOKEN() || ''
+
+  const onDone = (data: any) => {
+    const newImgs = [...imgs, data.name].slice(-3)
+    setImgs(newImgs)
+  }
 
   // TODO: 上传图片 可能要适配 rn 端
   const props: UploadProps = {
@@ -35,13 +41,18 @@ export default function ({ setImgs, imgs }: IProps) {
       'Blade-Auth': `${userInfo?.token_type || 'Bearer'} ${token}`
     },
     accept: 'image/png, image/jpeg, image/jpg',
-    // beforeUpload: (file) => {
-    //   const isLt1MB = file.size / 1024 / 1024 <= 1
-    //   if (!isLt1MB) {
-    //     message.info(`${intl.formatMessage({ id: 'mt.kycUploadImgSizeTips' }, { size: 1 })}MB`)
-    //   }
-    //   return isLt1MB
-    // },
+
+    beforeUpload: (file) => {
+      if (window?.ReactNativeWebView) {
+        window.ReactNativeWebView?.postMessage(
+          JSON.stringify({
+            type: 'takePhoto'
+          })
+        )
+        return false
+      }
+      return true
+    },
     onChange(info) {
       console.log('info', info)
       const data = info?.file?.response?.data || {}
@@ -51,12 +62,9 @@ export default function ({ setImgs, imgs }: IProps) {
       }
       if (status === 'done') {
         console.log('data', data)
-        // message.success(`${info.file.name} file uploaded successfully.`)
-        setFileName(data.name)
-        setUrl(data.link)
-
-        setImgs([...imgs.slice(-2), data.name])
+        onDone(data)
       } else if (status === 'error') {
+        console.log('error', info)
         message.info(`${info.file.name} file upload failed.`)
       }
     },
@@ -64,15 +72,64 @@ export default function ({ setImgs, imgs }: IProps) {
       console.log('Dropped files', e.dataTransfer.files)
     }
   }
+
+  const [times, setTimes] = useState(0)
+  const messageHandler = useCallback(
+    (e: any) => {
+      try {
+        const data = e?.data ? JSON.parse(e?.data) : undefined
+        console.log('======data====', data)
+        if (data?.action === 'upload' && data.times === times) {
+          setTimes((prev) => prev + 1)
+          onDone(data?.value)
+          window.ReactNativeWebView?.postMessage(
+            // 給 rn 端发送消息，停止接收照片
+            JSON.stringify({
+              type: 'stopReceivePhoto'
+            })
+          )
+        }
+      } catch (error) {
+        // message.info(`监听消息错误: ${JSON.stringify(error)}`)
+      }
+    },
+    [onDone]
+  )
+
+  useEffect(() => {
+    if (isIOS) {
+      window.addEventListener('message', messageHandler)
+    } else if (isAndroid) {
+      document.addEventListener('message', messageHandler)
+    }
+
+    return () => {
+      if (isIOS) {
+        window.removeEventListener('message', messageHandler)
+      } else if (isAndroid) {
+        document.removeEventListener('message', messageHandler)
+      }
+    }
+  }, [messageHandler])
+
   return (
     <>
       <Dragger {...props}>
-        {/* {url ? (
-          <div className="flex items-center justify-center flex-1">
-            <img src={url} width={347} height={235} />
-          </div>
-        ) : ( */}
-        <div className="flex items-center justify-center">
+        <div
+          className="flex items-center justify-center"
+          onClick={(e) => {
+            // e.preventDefault()
+            if (window?.ReactNativeWebView) {
+              e.stopPropagation()
+              window.ReactNativeWebView?.postMessage(
+                JSON.stringify({
+                  type: 'takePhoto',
+                  times
+                })
+              )
+            }
+          }}
+        >
           <div className="flex flex-col items-center justify-center bg-cover  h-[235px]">
             <img src="/img/upload-01.png" width={72} height={72} />
             <span className="text-primary text-sm font-semibold">
@@ -86,13 +143,14 @@ export default function ({ setImgs, imgs }: IProps) {
         {imgs.map((img, index) => (
           <div className="w-[86px] h-[86px] bg-gray-120 rounded-lg relative img-preview" key={index}>
             <Image src={`${getEnv().imgDomain}${img}`} width={86} height={86} />
-            <img
+            <div
               onClick={() => {
                 setImgs(imgs.filter((_, i) => i !== index))
               }}
-              src="/img/shanchu.png"
               className="absolute -top-2.5 -right-2.5 bg-secondary h-[24px] w-[24px] rounded-full cursor-pointer guanbi"
-            />
+            >
+              <img src="/img/shanchu.png" className="w-[24px] h-[24px]" />
+            </div>
             {/* <Iconfont name="danchuang-guanbi" className="text-secondary" size={18} /> */}
           </div>
         ))}
