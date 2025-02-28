98f8f67833e6b71b6c12148e1ed676d572fe48c9
poetries <jingguanliuye@gmail.com>
2025-02-28 14:16:06 +0800

    fix: 去掉access.ts canAdmin限制

--- src/access.ts
+++ src/access.ts
@@ -1,5 +1,3 @@
-import { STORAGE_GET_TOKEN } from './utils/storage'
-
 /**
  * @see https://umijs.org/zh-CN/plugins/plugin-access
  * */
@@ -8,7 +6,9 @@ export default function access(initialState: { currentUser?: User.UserInfo } | u
   return {
     // 登录可见
     // canAdmin: currentUser && currentUser.appId
-    canAdmin: !!STORAGE_GET_TOKEN()
+    // canAdmin: !!STORAGE_GET_TOKEN()
+    // 不在拦截跳转403页面，没有token触发接口请求401直接跳转登录页面
+    canAdmin: true
     // canUpdateFoo: role === 'admin',
     // canDeleteFoo: (foo) => {
     //   return foo.ownerId === userId;
