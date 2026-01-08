d24e7038ceb462f6d4c2f832c75bdae59dae0912
jokerrrr-wang <yzbingchuan@gmail.com>
2026-01-08 17:53:37 +0800

    feat: add global deposit monitor for all chains

--- src/layouts/main-layout/index.tsx
+++ src/layouts/main-layout/index.tsx
@@ -4,25 +4,29 @@ import { MainLayoutHeader } from './_layout/header'
 import React from 'react'
 // import Header from '@/components/Web/Header'
 import { ServerWalletsProvider } from '@/context/ServerWalletsProvider'
+import { GlobalDepositMonitorProvider } from '@/context/GlobalDepositMonitor'
 
 import { Outlet } from '@umijs/max'
 
 /**
  * MainLayout
  * 🔥 ServerWalletsProvider: 在进入主布局时预加载所有链的钱包地址
+ * 🔥 GlobalDepositMonitorProvider: 全局监听充值，持续检测新充值事件
  */
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <ServerWalletsProvider>
-      <div className="relative flex min-h-screen flex-col bg-secondary">
-        <MainLayoutHeader />
+      <GlobalDepositMonitorProvider>
+        <div className="relative flex min-h-screen flex-col bg-secondary">
+          <MainLayoutHeader />
 
-        <div className="flex flex-1">
-          <div className="w-full">
-            <Outlet />
+          <div className="flex flex-1">
+            <div className="w-full">
+              <Outlet />
+            </div>
           </div>
         </div>
-      </div>
+      </GlobalDepositMonitorProvider>
     </ServerWalletsProvider>
   )
 }
