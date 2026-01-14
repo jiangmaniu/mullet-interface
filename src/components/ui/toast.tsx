b920db595ec75393c816bb74428326c108f3cb1b
shugetsu <qiuliang_fang@163.com>
2026-01-14 16:53:39 +0800

    refactor: update toast component styles and replace icons with Iconify for improved UI consistency

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from '@/utils/cn'
 import { Toaster as Sonner, toast, ToasterProps } from 'sonner'
-import { Icons } from './icons'
+import { Iconify } from '@/libs/ui/components/icons'
 
 const Toaster = ({ ...props }: ToasterProps) => {
   return (
@@ -12,15 +12,18 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         unstyled: true,
         classNames: {
-          toast: cn('bg-[#0E123A] w-[var(--width)] gap-1.5 flex items-start border border-[#3B3D52] rounded-[8px] p-4'),
-          title: cn('text-[16px] text-white'),
-          description: cn('text-[14px] text-[#9FA0B0]'),
+          toast: cn(
+            'bg-pop-up-mask w-[var(--width)] gap-2 flex items-start border border-brand-default rounded-small p-2xl backdrop-blur-base'
+          ),
+          title: cn('text-paragraph-p2 text-white leading-6'),
+          description: cn('text-paragraph-p3 text-content-4'),
           content: cn('flex flex-col gap-2 flex-1'),
           closeButton: cn(
-            'order-last text-[#D8D8D8] p-0.5 size-5 justify-center items-center flex leading-none bg-[#3B3D52] rounded-full active:scale-95 transition-transform'
+            'order-last text-brand-secondary-3 p-0.5 size-6 justify-center items-center flex leading-none rounded-full active:scale-95 transition-transform'
           ),
           success: cn('text-[#2EBC84]'),
           error: cn('text-[#FF453A]'),
+          warning: cn('text-[#FFD60A]'),
           info: cn('text-[#2167ff]'),
           icon: cn('flex')
         },
@@ -35,7 +38,11 @@ const Toaster = ({ ...props }: ToasterProps) => {
         } as React.CSSProperties
       }
       icons={{
-        close: <Icons.lucide.Close size={14} />
+        close: <Iconify icon="iconoir:xmark" className="size-6" />,
+        success: <Iconify icon="iconoir:check-circle-solid" className="size-6" />,
+        error: <Iconify icon="iconoir:xmark-circle-solid" className="size-6" />,
+        info: <Iconify icon="iconoir:info-circle-solid" className="size-6" />,
+        warning: <Iconify icon="iconoir:info-circle-solid" className="size-6" />
       }}
       {...props}
     />
